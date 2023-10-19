8cf25e32abb05e4cdbe93dd39c34f010aec92916
Jo <jose.garnil27@gmail.com>
2023-10-19 15:52:07 +0200

    suppr Trait de tous les crudControllers ... pb de clear-cache et plantages

--- src/Controller/Admin/BookingCrudController.php
+++ src/Controller/Admin/BookingCrudController.php
@@ -3,15 +3,26 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Booking;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class BookingCrudController extends AbstractCrudController
 {
-    use Trait\readonlyTrait;
     public static function getEntityFqcn(): string
     {
         return Booking::class;
     }
+
+    public function configureActions(Actions $actions): Actions
+{
+    return $actions
+        // ...
+        ->add(Crud::PAGE_INDEX, Action::DETAIL)
+        ->add(Crud::PAGE_EDIT, Action::SAVE_AND_ADD_ANOTHER)
+    ;
+}
 
     /*
     public function configureFields(string $pageName): iterable
