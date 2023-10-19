f6f2b69529139ab7a007f1e82cff9dd1853b701f
Jo <jose.garnil27@gmail.com>
2023-10-19 10:52:38 +0200

    PageController : ajout route vers userDashboard

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\RoomRepository;
+use App\Repository\BookingRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PageController extends AbstractController
 {
-    #[Route('/userdashboard', name: 'app_page')]
-    public function index(): Response
+    #[Route('/userDashboard', name: 'app_userDashboard')]
+    public function dashboard(BookingRepository $bookingRepository, RoomRepository $rRepo): Response
     {
-        return $this->render('page/index.html.twig', [
+        $aRooms = $rRepo->findAll();
+        $countBookings = $bookingRepository->countBookingsByOwner($this->getUser());
+        return $this->render('page/userDashboard.html.twig', [
+            'aRooms' => $aRooms,
+            'countBookings' => $countBookings,
             'controller_name' => 'PageController',
+            'user' => $this->getUser(),
         ]);
     }
 }
