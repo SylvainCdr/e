e0a836f4d254cc5db8e2775bcba79fad0553f232
diskala <143798335+diskala@users.noreply.github.com>
2023-10-23 15:52:13 +0200

    commmit

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace App\Controller;
+
+use App\Form\SearchFormType;
 use App\Repository\RoomRepository;
 use App\Repository\BookingRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,8 +13,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class PageController extends AbstractController
 {
     #[Route('/userDashboard', name: 'app_userDashboard')]
-    public function dashboard(BookingRepository $bookingRepository, RoomRepository $rRepo): Response
+    public function dashboard(BookingRepository $bookingRepository, RoomRepository $rRepo, Request $request): Response
     {
+
+        
+        $form = $this->createForm(SearchFormType::class);
+        $form->handleRequest($request);
+       
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+           
+
+             
+
+           
+            
+           
+            // do anything else you need here, like send an email
+
+            // return $userAuthenticator->authenticateUser(
+            //     $user,
+            //     $authenticator,
+            //     $request
+            // );
+        }
+
+
+
  
         $aRooms = $rRepo->findAll();
         $countBookings = $bookingRepository->countBookingsByOwner($this->getUser());
@@ -27,21 +56,29 @@ class PageController extends AbstractController
             'reserved' => $reserved,
             'prereserved' => $prereserved,
             'cancelled' => $cancelled,
+            'searchForm'=>$form->createView()
         ]);
-    }
- 
+
+
+         
+               
+                
+               
+                
     
 
  
-    #[Route('/about', name: 'app_about')]
-    public function about()
-    {
-        
-        return $this->render('page/about.html.twig');
-    }
+   
  
 }
 
+#[Route('/about', name: 'app_about')]
+public function about()
+{
+    
+    return $this->render('page/about.html.twig');
+}
 
+}
 
  
--- src/Form/SearchFormType.php
+++ src/Form/SearchFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,15 @@ class SearchFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('motCles', SearchType::class)
+            ->add('motCles', SearchType::class,
+            [
+                'label'=> false,
+                'attr'=>[
+                    'class'=>'form-control',
+                    'placeholder'=>'Entrez un Mots clès'
+                ]
+
+            ])
         ;
     }
 
